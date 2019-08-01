7755be84a2a398ee93661af925996459363c7158
arvindvepa <arvind.m.vepa@gmail.com>
2019-07-31 18:09:16 -0700

    update

--- configs/mask_rcnn_x101_64x4d_fpn_1x_phtest2.py
+++ configs/mask_rcnn_x101_64x4d_fpn_1x_phtest2.py
@@ -142,6 +142,17 @@ data = dict(
         flip_ratio=0,
         with_mask=True,
         with_crowd=True,
+        with_label=True),
+    test=dict(
+        type=dataset_type,
+        ann_file=data_root + 'anns_rle.json',
+        img_prefix=data_root + 'fpl_data/',
+        img_scale=(1333, 800),
+        img_norm_cfg=img_norm_cfg,
+        size_divisor=32,
+        flip_ratio=0,
+        with_mask=True,
+        with_crowd=True,
         with_label=True))
 # optimizer
 optimizer = dict(type='SGD', lr=0.0025, momentum=0.9, weight_decay=0.0001)
