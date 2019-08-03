85b61ad5d34b24944d8a2af4ebafc88c9409a7ea
arvindvepa <arvind.m.vepa@gmail.com>
2019-08-03 01:00:54 -0700

    add test inference

--- configs/mask_rcnn_x101_64x4d_fpn_1x_pre_fpl_8_gpus.py
+++ configs/mask_rcnn_x101_64x4d_fpn_1x_pre_fpl_8_gpus.py
@@ -142,7 +142,19 @@ data = dict(
         flip_ratio=0,
         with_mask=True,
         with_crowd=True,
-        with_label=True))
+        with_label=True),
+    test=dict(
+        type=dataset_type,
+        ann_file=data_root + 'anns_rle.center-crop-all-1b-1c-val_v2-test.json',
+        img_prefix=data_root + 'data/',
+        img_scale=(1333, 800),
+        img_norm_cfg=img_norm_cfg,
+        size_divisor=32,
+        flip_ratio=0,
+        with_mask=True,
+        with_crowd=True,
+        with_label=True)
+)
 # optimizer
 optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
