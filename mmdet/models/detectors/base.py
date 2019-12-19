585b133ff595320bc0cd821e59f1f733e55521e2
arvind <arvind.m.vepa@gmail.com>
2019-12-19 12:56:49 -0800

    bug

--- mmdet/models/detectors/base.py
+++ mmdet/models/detectors/base.py
@@ -164,7 +164,7 @@ class BaseDetector(nn.Module):
                 for i, bbox in enumerate(bbox_result)
             ]
             labels = np.concatenate(labels)
-            print(bbox_result.shape)
+            print(len(bbox_result))
             print(labels.shape)
             print(bboxes.shape)
             mmcv.imshow_det_bboxes(
